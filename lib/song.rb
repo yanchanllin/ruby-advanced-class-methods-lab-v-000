f33fa2969dc547171f301ef04b6d2ae7578cafaf
YanC Lin <yonalin1@gmail.com>
2017-11-08 20:42:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,66 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.each do |song|
+      if song.name == name
+        return song
+      end
+    end
+  end
+
+  def self.find_or_create_by_name(find_song)
+    if self.find_by_name(name) || self.create_by_name(name) == nil
+      song = self.new
+      song.name = find_song
+      song.save
+      song
+    else
+    self.find_by_name(name) || self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename(filename)
+     line = filename
+    data = line.split(" - ")
+    artist_name = data[0]
+    song_name = data[1].gusb(".mp3", "")
+
+    song = self.new
+    artist_name = song.artist_name
+    song_name = song.name
+    song
+  end
+
+  def create_from_filename(filename)
+    self.new_from_filename(filename).save
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
