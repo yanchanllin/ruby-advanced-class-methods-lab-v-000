c5c476d1a68856cff0b15c5f8700febce84d842f
YanC Lin <yonalin1@gmail.com>
2017-11-08 21:28:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,18 +37,18 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-
-    if self.find_by_name(name)
-      return true
+self.find_by_name(name) || self.create_by_name(name)
+    # if self.find_by_name(name)
+    #   return true
     # elsif self.find_by_name(name) == nil
     #   song = self.new
     #   song.name = name
     #   song.save
     #   song
-    else self.find_by_name(name) == nil || false
-      self.create_by_name(name)
+    # else self.find_by_name(name) == nil || false
+    #   self.create_by_name(name)
 
-    end
+    # end
   end
 
 
